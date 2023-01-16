e3460bea027db28936f2c71bf05a58d4c08fd4b8
BialkowskiAleks <irisvoneverecc@gmail.com>
2023-01-16 17:35:42 +0100

    zadanie28

---
+++ zadanie28/main.js
@@ -0,0 +1,14 @@
+let myNumber = 12;
+
+if (myNumber > 10) {
+}
+
+if (myNumber > 10) {
+	console.log("Podana liczba jest większa od 10");
+}
+
+if (myNumber > 10) {
+	console.log("Podana liczba jest większa od 10");
+} else {
+	console.log("Podana liczba nie jest większa od 10");
+}
