20ec0ead17f39602e89b44a716ab10e616c4603c
BialkowskiAleks <irisvoneverecc@gmail.com>
2023-01-16 18:09:58 +0100

    zadanie29

---
+++ zadanie29/main.js
@@ -0,0 +1,10 @@
+let firstNumber = 8;
+let secondNumber = 10;
+
+function sumNumbers(num1, num2) {
+	let sum = num1 + num2;
+	return sum;
+}
+let result = sumNumbers(firstNumber, secondNumber);
+
+console.log(result);
